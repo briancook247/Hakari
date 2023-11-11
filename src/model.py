ea84335e2daaccb8bf36692328c358ec6e136b9f
JordanS2025 <97244972+JordanS2025@users.noreply.github.com>
2023-11-11 15:51:28 -0700

    example message

--- src/model.py
+++ src/model.py
@@ -33,3 +33,42 @@ if __name__ == "__main__":
 
     # Save the model to a file for later use in training and evaluation
     rnn_model.save("models/rnn_model.h5")
+    
+    
+    
+    
+    
+    # This the version i was working on but we can further the necessary changes. 
+
+# import tensorflow as tf
+# from tensorflow.keras import layers, models
+
+# def create_poker_model():
+#     # Create a Sequential model
+#     model = models.Sequential()
+
+#     # Input layer
+#     # Assuming a one-hot encoded representation of a poker hand (52 cards in a deck)
+#     model.add(layers.InputLayer(input_shape=(52,)))
+
+#     # Hidden layers
+#     # Dense layer with 128 neurons and ReLU activation
+#     model.add(layers.Dense(128, activation='relu'))
+
+#     # Dropout layer for regularization (prevents overfitting)
+#     model.add(layers.Dropout(0.5))
+
+#     # Dense layer with 64 neurons and ReLU activation
+#     model.add(layers.Dense(64, activation='relu'))
+
+#     # Output layer
+#     # Dense layer with softmax activation for multi-class classification
+#     # Assuming 10 classes representing different poker hands
+#     model.add(layers.Dense(10, activation='softmax'))
+
+#     return model
+
+# # Example usage:
+# # Create an instance of the poker model
+# poker_model = create_poker_model()
+
